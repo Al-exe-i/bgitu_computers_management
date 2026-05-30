46315fece577f962fd64c604cc1d1a2ff60b016d
Alexei <ax.mishin@yandex.ru>
2026-05-30 19:06:43 +0300

    Add UUIDPrimaryKeyMixin for UUID primary key support; implement mapped column for PostgreSQL UUID integration in uuid_mixin.py.

---
+++ models/mixins/uuid_mixin.py
@@ -0,0 +1,14 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from uuid import UUID, uuid4
+
+from sqlalchemy.dialects.postgresql import UUID as PGUUID
+
+
+class UUIDPrimaryKeyMixin:
+    """Примесь с UUID-первичным ключом"""
+
+    id: Mapped[UUID] = mapped_column(
+        PGUUID(as_uuid=True),
+        primary_key=True,
+        default=uuid4,
+    )
